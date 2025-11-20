d43d3d9a78e612fa1f990732444d89e3a3a1523f
ZeroMks <danielsanzjarillo.stn@gmail.com>
2025-11-20 09:24:52 +0100

    s

--- src/app/features/cliente-list/cliente-list.component.ts
+++ src/app/features/cliente-list/cliente-list.component.ts
@@ -1,22 +1,46 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { RouterLink } from '@angular/router';
 import { ClienteService } from '../../core/services/cliente.service';
-
 import { Cliente } from '../../core/models/cliente.models';
-import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-cliente-list',
   standalone: true,
-  imports: [CommonModule, RouterLink], 
+  imports: [CommonModule, RouterLink],
   templateUrl: './cliente-list.component.html',
   styleUrl: './cliente-list.component.scss'
 })
-export class ClienteListComponent {
+export class ClienteListComponent implements OnInit {
+  
   private clienteService = inject(ClienteService);
 
- 
-  clientes = toSignal(this.clienteService.getClientes(), { initialValue: [] });
-  
-} 
+  // CAMBIO: Usamos un signal "normal" para poder actualizarlo nosotros
+  clientes = signal<Cliente[]>([]);
+
+  ngOnInit() {
+    this.cargarDatos();
+  }
+
+  // Método para pedir los datos al servicio
+  cargarDatos() {
+    this.clienteService.getClientes().subscribe(data => {
+      this.clientes.set(data); // <--- Aquí actualizamos la señal
+    });
+  }
+
+  // Método para borrar
+  borrarCliente(id: number) {
+    // 1. Preguntar confirmación (Nativo del navegador)
+    if (confirm('¿Estás seguro de que quieres borrar este cliente?')) {
+      
+      // 2. Llamar al servicio
+      this.clienteService.deleteCliente(id).subscribe(() => {
+        
+        // 3. Al terminar, recargamos la lista para que desaparezca
+        this.cargarDatos();
+        
+      });
+    }
+  }
+}
