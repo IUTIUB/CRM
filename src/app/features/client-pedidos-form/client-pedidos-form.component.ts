94f59766ff047e97021f13c5e57339e340c93cf2
IUTIUB <jonzalomateossanchez@gmail.com>
2025-11-25 19:52:56 +0100

    1234

--- src/app/features/client-pedidos-form/client-pedidos-form.component.ts
+++ src/app/features/client-pedidos-form/client-pedidos-form.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-client-pedidos-form',
   standalone: true,
-  imports: [],
-  templateUrl: './client-pedidos-form.component.html',
-  styleUrl: './client-pedidos-form.component.css'
+  imports: [RouterLink],
+  templateUrl: './client-pedidos-form.component.html', // Ahora sí usamos el archivo externo
+  styleUrl: './client-pedidos-form.component.css'      // Y el CSS externo
 })
 export class ClientPedidosFormComponent {
 
